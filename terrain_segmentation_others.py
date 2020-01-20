a357b839d38389c90e4d1df4a1629c825c64e593
vmalepati1 <malepativikas@gmail.com>
2020-01-20 14:13:37 -0500

    Updated fpn training

--- terrain_segmentation_others.py
+++ terrain_segmentation_others.py
@@ -9,14 +9,16 @@ from PIL import Image
 # Number of classes (including background)
 from keras.utils import Sequence
 from skimage.draw import polygon
+from google.colab import drive
 import matplotlib.pyplot as plt
 from matplotlib.image import imread
-
+drive.mount('/content/gdrive')
 from matplotlib.pyplot import figure
 import albumentations as A
-
-figure(num=None, figsize=(8, 8), dpi=80, facecolor='w', edgecolor='k')
-
+import keras.backend as K
+import sys
+import math
+# figure(num=None, figsize=(8, 8), dpi=80, facecolor='w', edgecolor='k')
 
 class DataGenerator(Sequence):
     """Generates data for Keras
@@ -47,7 +49,6 @@ class DataGenerator(Sequence):
         self.file_names = os.listdir(self.image_path)  # List of training image file names
         self.file_names.remove('via_region_data.json')
         self.annotations = json.load(open(os.path.join(self.image_path, "via_region_data.json"))).values()
-        self.class_values = [self.classes.index(cls) for cls in self.classes]
 
         self.on_epoch_end()
 
@@ -67,6 +68,9 @@ class DataGenerator(Sequence):
         ]
         return A.Compose(_transform)
 
+    def _round_clip_0_1(self, x):
+        return x.round().clip(0, 1)
+
     def __len__(self):
         """Denotes the number of batches per epoch
         :return: number of batches per epoch
@@ -85,7 +89,7 @@ class DataGenerator(Sequence):
         file_names_temp = [self.file_names[k] for k in indexes]
 
         # Generate data
-        X = self.preprocess_input(self._generate_X(file_names_temp))
+        X = self._generate_X(file_names_temp)
 
         if self.to_fit:
             y = self._generate_y(file_names_temp)
@@ -107,16 +111,14 @@ class DataGenerator(Sequence):
         """
         X = []
 
-        print(list_file_names)
-
         # Generate data
-        for i, filepath in enumerate(list_file_names):
+        for filepath in list_file_names:
             # Store normalized sample
             orig = Image.open(os.path.join(self.image_path, filepath))
             new = orig.resize(self.dim)
             new_rgb = new.convert('RGB')
-            image = np.asarray(new_rgb, 'int')
-            sample = self.preprocess_input(image=image)
+            im = np.asarray(new_rgb, 'int')
+            sample = self.preprocess_input(image=im)
             X.append(sample['image'])
 
         return np.array(X)
@@ -127,8 +129,6 @@ class DataGenerator(Sequence):
         :return: batch of masks
         """
         y = []
-
-        print(list_file_names)
 
         # Load annotations
         # VGG Image Annotator saves each image in the form:
@@ -146,7 +146,7 @@ class DataGenerator(Sequence):
         # }
 
         # Generate data
-        for i, filename in enumerate(list_file_names):
+        for filename in list_file_names:
             # Store sample
             a = next(k for k in self.annotations if k['filename'] == filename)
 
@@ -158,26 +158,24 @@ class DataGenerator(Sequence):
                 for poly in polygons:
                     xp = [int(x * self.mask_scale_factor) for x in poly['shape_attributes']['all_points_x']]
                     yp = [int(y * self.mask_scale_factor) for y in poly['shape_attributes']['all_points_y']]
-                    rr, cc = polygon(xp, yp)
+                    rr, cc = polygon(yp, xp)
                     class_id = self.classes.index(poly['region_attributes']['building'])
-                    mask[rr, cc, class_id] = class_id
+                    mask[rr, cc, class_id] = 1
 
             # add background if mask is not binary
             if mask.shape[-1] != 1:
                 background = 1 - mask.sum(axis=-1, keepdims=True)
                 mask = np.concatenate((mask, background), axis=-1)
 
-            y.append(mask)
+            y.append(self._round_clip_0_1(mask))
 
         return np.array(y)
 
-
-TRAINING_IMAGE_DIR = 'deeproof-release/data/final-dataset/train'
-VAL_IMAGE_DIR = 'deeproof-release/data/final-dataset/val'
+TRAINING_IMAGE_DIR = '/content/gdrive/My Drive/SolarGenie/deeproof-release/data/final-dataset/train'
+VAL_IMAGE_DIR = '/content/gdrive/My Drive/SolarGenie/deeproof-release/data/final-dataset/val'
 
 classes = [
     "flat",
-    "dome",
     "N",
     "NNE",
     "NE",
@@ -197,10 +195,8 @@ classes = [
     "tree",
 ]
 
-
 def get_num_files_in_dir(dir):
     return len([name for name in os.listdir(dir) if os.path.isfile(os.path.join(dir, name))])
-
 
 NUMBER_OF_TRAINING_IMAGES = get_num_files_in_dir(TRAINING_IMAGE_DIR) - 1
 NUMBER_OF_VAL_IMAGES = get_num_files_in_dir(VAL_IMAGE_DIR) - 1
@@ -208,68 +204,78 @@ print(NUMBER_OF_TRAINING_IMAGES)
 print(NUMBER_OF_VAL_IMAGES)
 IMAGE_DIM = 512
 BATCH_SIZE = 2
-LR = 0.0001
+LR = 0.001
+
+def lr_decay(epoch):
+    if epoch < 200:
+        return LR
+    elif epoch >= 200 and epoch < 320:
+        return LR / 10
+    else:
+        return LR / 100
+
+# Ideal calculations
+# STEPS_PER_EPOCH = NUMBER_OF_TRAINING_IMAGES // BATCH_SIZE
+# VAL_STEPS = NUMBER_OF_VAL_IMAGES // BATCH_SIZE
+
+# "Slow-cook" dataset
+STEPS_PER_EPOCH = 500
+VAL_STEPS = 50
 
 n_classes = 1 if len(classes) == 1 else (len(classes) + 1)  # case for binary and multiclass segmentation
 activation = 'sigmoid' if n_classes == 1 else 'softmax'
 
-models = {
-    # name, model instance
-    'fpn_resnet101': sm.FPN('resnet101', classes=n_classes, encoder_weights='imagenet', activation=activation),
-    'unet_resnet101': sm.Unet('resnet101', classes=n_classes, encoder_weights='imagenet', activation=activation),
-    'fpn_resnet50': sm.FPN('resnet50', classes=n_classes, encoder_weights='imagenet', activation=activation),
-    'unet_resnet50': sm.Unet('resnet50', classes=n_classes, encoder_weights='imagenet', activation=activation),
-}
+print(n_classes)
 
-for name, model in models.items():
-    # define optomizer
-    optim = keras.optimizers.Adam(LR)
+backbone = 'resnet101'
+name = 'fpn_' + backbone
+model = sm.FPN(backbone, classes=n_classes, activation=activation, encoder_weights='imagenet', pyramid_aggregation='concat')
 
-    # Segmentation models losses can be combined together by '+' and scaled by integer or float factor
-    # set class weights for dice_loss (car: 1.; pedestrian: 2.; background: 0.5;)
-    class_weights = np.array(
-        [2.36465827, 0.78821942, 0.78821942, 1.01760836, 1.00554493, 0.88297515,
-         0.99376417, 1.01682135, 1.00592961, 0.88327175, 0.99451589, 1.01682135,
-         1.00516055, 0.88297515, 0.99413989, 1.0172147, 1.00516055, 0.88297515,
-         0.99376417, 1.7390873]
-    )
+# define optomizer
+optim = keras.optimizers.SGD(LR, momentum=0.9)
 
-    dice_loss = sm.losses.DiceLoss(class_weights=class_weights)
-    focal_loss = sm.losses.BinaryFocalLoss() if n_classes == 1 else sm.losses.CategoricalFocalLoss()
-    total_loss = dice_loss + (1 * focal_loss)
+# Segmentation models losses can be combined together by '+' and scaled by integer or float factor
+# set class weights for dice_loss (car: 1.; pedestrian: 2.; background: 0.5;)
+class_weights = [0.41432617, 1.08753001, 1.09721607, 3.24666718, 1.13021085, 1.08753001,
+ 1.09721607, 3.24666718, 1.13021085, 1.08753001, 1.09721607, 3.24666718,
+ 1.13021085, 1.08753001, 1.09721607, 3.24666718, 1.13021085, 0.32764091,
+ 0.69469727]
 
-    # actulally total_loss can be imported directly from library, above example just show you how to manipulate with losses
-    # total_loss = sm.losses.binary_focal_dice_loss # or sm.losses.categorical_focal_dice_loss
+dice_loss = sm.losses.DiceLoss(class_weights=class_weights)
+ce_loss = sm.losses.CategoricalCELoss(class_weights=class_weights)
+total_loss = dice_loss + ce_loss
 
-    metrics = [sm.metrics.IOUScore(threshold=0.5), sm.metrics.FScore(threshold=0.5)]
+# actulally total_loss can be imported directly from library, above example just show you how to manipulate with losses
+# total_loss = sm.losses.binary_focal_dice_loss # or sm.losses.categorical_focal_dice_loss
 
-    # compile keras model with defined optimozer, loss and metrics
-    model.compile(optim, total_loss, metrics)
+metrics = [sm.metrics.IOUScore(threshold=0.5), sm.metrics.FScore(threshold=0.5)]
 
-    # Callbacks
-    callbacks = [
-        keras.callbacks.TensorBoard(log_dir="/content/gdrive/My Drive/SolarGenie/models/others_logs/{}".format(time()),
-                                    histogram_freq=0, write_graph=True, write_images=False),
-        keras.callbacks.ModelCheckpoint('/content/gdrive/My Drive/SolarGenie/models/' + name + '_weights.latest.h5',
-                                        verbose=1, save_weights_only=True),
-        keras.callbacks.ModelCheckpoint('/content/gdrive/My Drive/SolarGenie/models/' + name + '_weights.best.h5',
-                                        verbose=1, save_best_only=True, save_weights_only=True, mode='min'),
-    ]
+# compile keras model with defined optimozer, loss and metrics
+model.compile(optim, total_loss, metrics)
 
-    print('Creating generators')
+# Callbacks
+callbacks = [
+    keras.callbacks.TensorBoard(log_dir="models/others_logs/{}".format(time()),
+                                histogram_freq=0, write_graph=True, write_images=False),
+    keras.callbacks.ModelCheckpoint('models/' + name +'_weights.latest.h5',
+                                    verbose=1, save_weights_only=True),
+    keras.callbacks.ModelCheckpoint('models/' + name +'_weights.best.h5',
+                            verbose=1, save_best_only=True, save_weights_only=True, mode='min'),
+    keras.callbacks.LearningRateScheduler(lr_decay)
+]
 
-    training_generator = DataGenerator(TRAINING_IMAGE_DIR, classes, 'resnet101', batch_size=BATCH_SIZE,
-                                       dim=(IMAGE_DIM, IMAGE_DIM), mask_scale_factor=IMAGE_DIM / 512)
-    validation_generator = DataGenerator(VAL_IMAGE_DIR, classes, 'resnet101', batch_size=BATCH_SIZE,
-                                         dim=(IMAGE_DIM, IMAGE_DIM), mask_scale_factor=IMAGE_DIM / 512)
+print('Creating generators')
 
-    print('Training ' + name)
-    model.fit_generator(
-        training_generator,
-        verbose=1,
-        epochs=40,
-        steps_per_epoch=NUMBER_OF_TRAINING_IMAGES // BATCH_SIZE,
-        callbacks=callbacks,
-        validation_data=validation_generator,
-        validation_steps=NUMBER_OF_VAL_IMAGES // BATCH_SIZE,
-    )
+training_generator = DataGenerator(TRAINING_IMAGE_DIR, classes, backbone, batch_size=BATCH_SIZE, dim=(IMAGE_DIM, IMAGE_DIM), mask_scale_factor=IMAGE_DIM/512)
+validation_generator = DataGenerator(VAL_IMAGE_DIR, classes, backbone, batch_size=BATCH_SIZE, dim=(IMAGE_DIM, IMAGE_DIM), mask_scale_factor=IMAGE_DIM/512)
+
+print('Training ' + name)
+model.fit_generator(
+    training_generator,
+    verbose=1,
+    epochs=480,
+    steps_per_epoch=STEPS_PER_EPOCH,
+    callbacks=callbacks,
+    validation_data=validation_generator,
+    validation_steps=VAL_STEPS,
+)
